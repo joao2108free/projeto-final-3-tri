9c689c0ad4f2a6ff07bf779477ca6ab23f4c746e
joao2108free <161027111+joao2108free@users.noreply.github.com>
2024-10-07 07:47:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'história',
+    'qual a cor do cavalo branco de napoleão?',
+    'cinza claro'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da finlândia?',
+    'Helsinque'
+)
+
+criaCartao(
+    'futebol',
+    'quem é o jogador com mais copas do mundo?',
+    'Pelé com 3'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz brasileiro em Inglês?',
+    'brazilian'
+)
